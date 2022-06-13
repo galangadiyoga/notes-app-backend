b46c73c1ac411c52b23bf6aa574bfd05c0382b91
galangadiyoga <97786485+galangadiyoga@users.noreply.github.com>
2022-06-13 07:47:37 +0700

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,10 @@
 const {
-  // postPlantHandler,
   getPlantHandler,
   getDetailPlantHandler,
-  // putPlantHandler,
-  // deletePlantHandler,
 } = require('./handler');
 
 const routes = [
-  // {
-  //  method: 'POST',
-  //  path: '/plant',
-  //  handler: postPlantHandler,
-  // },
+  
   {
     method: 'GET',
     path: '/plant',
@@ -24,16 +17,7 @@ const routes = [
     config: { auth: 'jwt' },
     handler: getDetailPlantHandler,
   },
-  // {
-  //  method: 'PUT',
-  //  path: '/plant/{id}',
-  //  handler: putPlantHandler,
-  // },
-  // {
-  //  method: 'DELETE',
-  //  path: '/plant/{id}',
-  //  handler: deletePlantHandler,
-  // },
+  
 ];
 
 module.exports = routes;
