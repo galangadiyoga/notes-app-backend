d46798a6deb2d7b1d3a500dd4496808afb7d101e
galangadiyoga <97786485+galangadiyoga@users.noreply.github.com>
2022-06-13 07:56:23 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,20 +1,6 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
-const Users = {
-  1: {
-    id: 1,
-    name: 'planT',
-  },
-};
-
-// eslint-disable-next-line func-names
-const validate = async function (decoded) {
-  if (Users[decoded.id]) {
-    return { isValid: true };
-  }
-  return { isValid: false };
-};
 
 const init = async () => {
   const Api = Hapi.server({
@@ -27,20 +13,11 @@ const init = async () => {
     },
   });
 
-  // eslint-disable-next-line global-require
-  await Api.register(require('hapi-auth-jwt2'));
-  Api.auth.strategy('jwt', 'jwt', { key: 'BurjoSariAsih', validate });
-
-  Api.auth.default('jwt');
-  Api.route(routes);
-
-  await Api.start();
-  return Api;
 };
 
 init()
   .then((Api) => {
-    console.log('Server beroperasi di', Api.info.uri);
+    console.log('Server berjalan di', Api.info.uri);
   })
   .catch((err) => {
     console.log(err);
